6403c1e949e78434fb447dc32231364f762fd2ea
JHANMER <jhanmer_pj@hotmail.com>
2023-04-30 18:50:20 -0500

    se agrego el file consultas y registro

---
+++ CConsultas.php
@@ -0,0 +1,157 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="Imagenes/IProductos/Inicio/LOGO.jpg">
+    <title>Doctor Pet</title>
+    <link href="CSS/EstiloBFila.css" rel="stylesheet" type="text/css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="CSS/EstiloC.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/EstiloBLateral.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/EstiloBotonSearch.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/EstiloHContenedor.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/EstiloPiePagina.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+    <link rel="stylesheet" href="CSS/normalize.css" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="preload" href="CSS/style.css" as="style" type="text/css">
+    <link rel="stylesheet" href="CSS/style.css" type="text/css">
+
+</head>
+
+<body>
+    <header>
+        <input type="checkbox" id="check">
+        <label for="check">
+            <i class="fas fa-bars" id="btn"></i>
+            <i class="fas fa-times" id="cancel"></i>
+        </label>
+        <div class="sidebar">
+            <h2>PRODUCTOS</h2>
+            <ul>
+                <li><a href="CVestimenta.php"><i class="fas fa-qrcode"></i>Vestimenta</a></li>
+                <li><a href="CMedicamentos.php"><i class="fas fa-qrcode"></i>Medicamentos</a></li>
+                <li><a href="CAlimentos.php"><i class="fas fa-qrcode"></i>Alimento</a></li>
+                <li><a href="CAccesorios.php"><i class="fas fa-qrcode"></i>Accesorios</a></li>
+            </ul>
+        </div>
+        <div class="logo">
+            <a href="#"><img src="Imagenes/LOGO.jpg" alt="" /></a>
+        </div>
+
+        <div class="Busqueda">
+            <input type="text" placeholder="Buscar">
+            <div class="btn">
+                <i class="fa fa-search"></i>
+            </div>
+        </div>
+
+        <div class="info-header">
+            <nav>
+                <a href="RegistroC.php">Registrate</a>
+                <a href="#">Iniciar Sesion</a>
+            </nav>
+        </div>
+
+    </header>
+    <main class="contendor sombra">
+        <div class="Barra-main">
+            <nav>
+                <ul class="menu-horizontal">
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Nosotros</a></li>
+                    <li>
+                        <a href="#">Productos</a>
+                        <ul class="menu-vertical">
+                            <li><a href="CVestimenta.php">Vestimenta</a></li>
+                            <li><a href="CMedicamentos.php">Medicamentos</a></li>
+                            <li><a href="CAlimentos.php">Alimentos</a></li>
+                            <li><a href="CAccesorios.php">Accesorios</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Servicios</a>
+                        <ul class="menu-vertical">
+                            <li><a href="CConsultas.php">Consultas</a></li>
+                            <li><a href="CBañosCortes.php">Baño y Corte</a></li>
+                            <li><a href="CCampañas.php">Campañas</a></li>
+                            <li><a href="CAnimalesPerdidos.php">Animales Perdidos</a></li>
+                            <li><a href="CAtencionDomicilio.php">Atencion a Domicilio</a></li>
+                            <li><a href="CConsultaInternet.php">Consultas online</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Adopciones</a></li>
+                </ul>
+            </nav>
+        </div>
+        <section>
+            <h2>Reservar cita</h2>
+            <form class="formulario">
+                <fieldset>
+                    <!-- <legend>Contáctanos llenando todos los campos</legend> -->
+                    <div class="contenedor-campos">
+                        <div class="campo">
+                            <label for="">Nombre</label>
+                            <input class="input-text" type="text" placeholder="Tu Nombre">
+                        </div>
+                        <div class="campo">
+                            <label for="">Teléfono</label>
+                            <input class="input-text" type="tel" placeholder="teléfono">
+                        </div>
+                        <div class="campo">
+                            <label for="">Fecha</label>
+                            <input class="input-text" type="date" placeholder="teléfono">
+                        </div>
+                        <div class="campo">
+                            <label for="">Correo</label>
+                            <input class="input-text" type="email" placeholder="email">
+                        </div>
+                        <div class="campo">
+                            <label for="">Motivo</label>
+                            <textarea class="input-text" placeholder="Cuéntanos tu asunto..."></textarea>
+                        </div>
+                    </div><!-- contenedor campos -->
+                    <div class="alinear-derecha flex">
+                        <input class="boton w-sm-100" type="submit" value="Enviar">
+                    </div>
+                </fieldset>
+            </form>
+        </section>
+    </main>
+    <footer class="footer">
+        <div class="container-f">
+            <div class="footer-row">
+                <div class="footer-links">
+                    <h4>Veterinaria</h4>
+                    <ul>
+                        <li><a href="#">Nosotros</a></li>
+                        <li><a href="#">Nuestra tienda</a></li>
+                        <li><a href="#">Trabaja con nosotros</a></li>
+                    </ul>
+                </div>
+                <div class="footer-links">
+                    <h4>Ayuda</h4>
+                    <ul>
+                        <li><a href="#">Medios de pagos</a></li>
+                        <li><a href="#">Preguntas frecuentes</a></li>
+                    </ul>
+                </div>
+                <div class="footer-links">
+                    <h4>Redes Sociales</h4>
+                    <div class="social-link">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-tiktok"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
---
+++ RegistroC.php
@@ -0,0 +1,94 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="Imagenes/IProductos/Inicio/LOGO.jpg">
+    <title>Doctor Pet</title>
+    <link href="CSS/EstiloBFila.css" rel="stylesheet" type="text/css" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="CSS/EstiloC.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/EstiloBLateral.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/EstiloBotonSearch.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/EstiloHContenedor.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/EstiloPiePagina.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+    <link href="CSS/EstiloRegistroClientes.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+            <div class="container-RC">
+            <div class="form-content-RC">
+                <h1 id="title">Registro</h1>
+                <form>
+                    <div class="input-group-RC">
+                        <div class="input-field-RC">
+                            <i class="fa-solid fa-user"></i>
+                            <input type="text" placeholder="Nombres">
+                        </div>
+                        <div class="input-field-RC">
+                            <i class="fa-solid fa-user"></i>
+                            <input type="text" placeholder="Apellidos">
+                        </div>
+                        <div class="input-field-RC">
+                            <i class="fa-solid fa-cake-candles"></i>
+                            <input type="date" min="1900-01-01">
+                        </div>
+                        <div class="input-field-RC-2">
+                            <i class="fa-solid fa-venus-mars"></i>
+                            <input type="radio" name="gender" value="Hombre">Hombre
+                            <input type="radio" name="gender" value="Muejr">Mujer
+                        </div>
+                        <div class="input-field-RC">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <input type="text" placeholder="Dirección">
+                        </div>
+                        <div class="input-field-RC-2">
+                            <i class="fa-solid fa-map-pin"></i>
+                            <div class="select-container">
+                            <select class="select-box">
+                                <option value="nada">Selecciona un distrito</option>
+                                <option value="1">Chorrillos</option>
+                                <option value="2">Pachacamac</option>
+                                <option value="3">Pucusana</option>
+                                <option value="4">San Juan de Miraflores</option>
+                                <option value="5">San Bartolo</option>
+                                <option value="6">Punta Hermosa</option>
+                                <option value="7">Punta Negra</option>
+                                <option value="8">Lurín</option>
+                                <option value="9">Santa María del Mar</option>
+                                <option value="10">Villa El Salvador</option>
+                                <option value="10">Villa María del Triunfo</option>
+                            </select>
+                                <div class="icon-container">
+                                <i class="fa-solid fa-caret-down"></i>
+                                </div>
+                            </div>
+                        </div>
+                         <div class="input-field-RC">
+                            <i class="fa-solid fa-phone"></i>
+                            <input type="text" placeholder="Teléfono">
+                        </div>
+                        <div class="input-field-RC">
+                            <i class="fa-solid fa-envelope"></i>
+                            <input type="email" placeholder="Correo">
+                        </div>
+                        <div class="input-field-RC">
+                            <i class="fa-solid fa-lock"></i>
+                            <input type="password" placeholder="Contraseña">
+                        </div>
+                        <p>Olvidaste tu contraseña <a href="#">click aquí</a></p>
+                    </div>
+                    <div class="btn-field-RC">
+                        <button id="signUp" type="button" onclick="alert('¡Gracias por registrarte!')">Registrarse</button>
+                        <button id="signIn" type="button"><a href="#">Login</a></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+</body>
+
+</html>
