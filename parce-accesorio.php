ae9885acfa9b34da40b44628a0c247f6fef9a51e
FrankRC1 <frankrua181119@gmail.com>
2023-06-26 21:10:38 -0500

    agregando animaciones arreglar las vistas, ordenar y optimizar la carga

--- parce-accesorio.php
+++ parce-accesorio.php
@@ -27,3 +27,68 @@ $resultado = mysqli_query($db, $query);
     <?php endwhile; ?>
 
 </section><!-- fin de productos para gatos -->
+
+<style>
+    .producto {
+        display: flex;
+        margin-bottom: 20px;
+    }
+
+    .producto-base {
+        width: 300px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        padding: 10px;
+        margin-bottom: 20px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        transition: box-shadow 0.3s ease;
+    }
+
+    .producto-base:hover {
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+    }
+
+    .imagen {
+        position: relative;
+        overflow: hidden;
+    }
+
+    .imagen img {
+        width: 100%;
+        height: auto;
+        margin-bottom: 10px;
+        transition: transform 0.3s ease;
+    }
+
+    .imagen:hover img {
+        transform: scale(1.3);
+    }
+
+    .texto {
+        text-align: center;
+    }
+
+    .texto h4 {
+        font-size: 18px;
+        margin-bottom: 5px;
+    }
+
+    .texto p {
+        font-size: 14px;
+        margin-bottom: 10px;
+    }
+
+    .agregar {
+        background-color: #007bff;
+        color: #fff;
+        padding: 5px 10px;
+        border: none;
+        border-radius: 3px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    .agregar:hover {
+        background-color: #0056b3;
+    }
+</style>
