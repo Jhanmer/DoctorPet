2afb052bc77aada16d6ca18f08b5359d758e4c2a
JHANMER <jhanmer_pj@hotmail.com>
2023-07-08 01:57:37 -0500

    reserva citas admin mejorado

--- admin-dashboard/TReservarCitas.php
+++ admin-dashboard/TReservarCitas.php
@@ -1,18 +1,24 @@
 <?php
 require '../Config/conexion_bd.php';
 $con = fnConnect($msg);
-$sql = "select cp.idConsultaPer,mas.NomMasc,vet.nombreVet, vet.apellidoVet, 
+$sql = "select cp.idConsultaPer,vet.nombreVet, vet.apellidoVet, 
 vet.especialidad,cp.motivo, cp.fechaAtencion,
 cp.monto, 
 Case cp.estadoPago WHEN 0 THEN 'Pago Pendiente' WHEN 1 THEN 'Pago Sin Validar' WHEN 2 THEN 'Pago Realizado' END AS EstadoPagoTexto, 
 Case cp.estadoAtencion WHEN 0 THEN 'Pendiente' WHEN 1 THEN 'En curso' WHEN 2 THEN 'Atendido' END AS EstadoAtencionTexto, 
-hor.hora from dp_consultapersonalizada cp inner join dp_veterinarios vet on cp.idConsultaPer=vet.idVeterinario 
-inner join dp_mascota mas on mas.idMascota=cp.idConsultaPer 
-inner join dp_hora hor on cp.idConsultaPer=hor.idHora;";
+hor.hora, mas.NomMasc
+from dp_consultapersonalizada cp 
+inner join dp_veterinarios vet on vet.idVeterinario=cp.idVeterinario
+inner join dp_hora hor on hor.idHora=cp.idHora
+inner join dp_mascota mas on mas.idMascota = cp.idMascota;";
 $lista= mysqli_query($con, $sql);
 
-$sqlEvidencia = "select idConsultaPer, pago.cartera, pago.dni, pago.evidenciaPago from dp_pagoconsultapersonalizada pago;";
+$sqlEvidencia = "select idConsultaPer, pago.cartera, pago.dni, pago.evidenciaPago, cli.nombre, cli.apellidos from dp_pagoconsultapersonalizada pago
+inner join dp_cliente cli on cli.idCliente=pago.idCliente;";
 $lista= mysqli_query($con, $sqlEvidencia);
+
+$sqlhora = "select hora.idHora, hora.hora, CASE hora.estado WHEN 0 THEN 'Hora no Diponible' WHEN 1 THEN 'Hora Disponible' END AS estadoHora from dp_hora hora;";
+$lista= mysqli_query($con, $sqlhora);
 
 include ('barra-lateral.php');
 ?>
@@ -73,7 +79,7 @@ include ('barra-lateral.php');
                                             <td  id="fila-<?php echo $row['idConsultaPer'];?>" ><?php echo $row['idConsultaPer']; ?></td>
                                             <td><?php echo $row['NomMasc']; ?> </td>
                                             <td><?php echo $row['nombreVet']; ?> </td>
-                                            <td> <?php echo $row['apellidoVet']; ?> años</td>
+                                            <td> <?php echo $row['apellidoVet']; ?></td>
                                             <td> <?php echo $row['especialidad']; ?></td>
                                             <td> <?php echo $row['motivo']; ?> </td>
                                             <td> <?php echo $row['fechaAtencion']; ?> </td>
@@ -141,51 +147,157 @@ include ('barra-lateral.php');
 
                 </section>
             </div>
-
+        </div>    
+ </div>
+ <div id="app">
+    <div id="main">
+        <div class="page-heading">            
             <section class="section">
-                    <div class="card">
-                        <div class="card-header">
-                            Tabla de Evidencias
+                    <div class="row" id="basic-table">
+                        <div class="col-12 col-md-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">Tabla de Evidencias</h4>
+                                </div>
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <p class="card-text">Por favor, se debe verificar
+                                            <code>imagen</code> detenidamente, además de verificar la constancia de yapeo en la aplicación.
+                                        </p>
+                                        <!-- Table with outer spacing -->
+                                        <div class="table-responsive">
+                                            <table class="table table-lg">
+                                            <thead>
+                                                <tr>
+                                                    <th>ID Reserva</th>
+                                                    <th>Cartera</th>
+                                                    <th>DNI</th>
+                                                    <th>Evidencia</th>
+                                                    <th>Nombre Cliente</th>
+                                                    <th>Apellido Cliente</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                        $busc= mysqli_query($con, $sqlEvidencia);
+
+                                                        if($busc -> num_rows >0){
+                                                            while($row= mysqli_fetch_array($busc)){                                       
+                                                        ?> 
+                                                        <tr>
+                                                            <td  id="fila-<?php echo $row['idConsultaPer'];?>" ><?php echo $row['idConsultaPer']; ?></td> 
+                                                            <td><?php echo $row['cartera']; ?></td>  
+                                                            <td><?php echo $row['dni']; ?></td>  
+                                                            <td>
+                                                                    <!-- Button trigger for Disabled Backdrop -->
+                                                                    <button type="button" class="btn btn-outline-primary block"
+                                                                        data-bs-toggle="modal" data-bs-backdrop="false" data-bs-target="#backdrop">
+                                                                        Ver Imagen
+                                                                    </button>
+
+                                                                    <!--Disabled Backdrop Modal -->
+                                                                    <div class="modal fade text-left" id="backdrop" tabindex="-1" role="dialog"
+                                                                        aria-labelledby="myModalLabel4" aria-hidden="true">
+                                                                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+                                                                            role="document">
+                                                                            <div class="modal-content">
+                                                                                <div class="modal-header">
+                                                                                    <h4 class="modal-title" id="myModalLabel4">Validar Pago
+                                                                                    </h4>
+                                                                                    <button type="button" class="close" data-bs-dismiss="modal"
+                                                                                        aria-label="Close">
+                                                                                        <i data-feather="x"></i>
+                                                                                    </button>
+                                                                                </div>
+                                                                                <div class="modal-body">
+                                                                                    <p>
+                                                                                        Por favor, revise detenidamente la información, como Fecha y Hora.
+                                                                                    </p>
+                                                                                    <div style="diplay: flex, display: flex; justify-content: center; align-items: center; text-align: center;">
+                                                                                        <img width="250px" height="500px" src="data:image/jpg;base64,<?php echo base64_encode($row['evidenciaPago']); ?>"/>                                      
+                                                                                    </div>    
+                                                                                </div>
+                                                                                <div class="modal-footer">
+                                                                                    <button type="button" class="btn btn-light-secondary"
+                                                                                        data-bs-dismiss="modal">
+                                                                                        <i class="bx bx-x d-block d-sm-none"></i>
+                                                                                        <span class="d-none d-sm-block">Cerrar</span>
+                                                                                    </button>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                            </td>
+                                                            <td><?php echo $row['nombre']; ?></td>  
+                                                            <td><?php echo $row['apellidos']; ?></td>  
+                                                        </tr>
+                                                    <?php
+                                                        }
+                                                    }
+
+                                                    ?>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <table class="table table-striped" id="table1">
-                                <thead>
-                                    <tr>
-                                        <th>ID Reserva</th>
-                                        <th>Cartera</th>
-                                        <th>DNI</th>
-                                        <th>Evidencia</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                        $busc= mysqli_query($con, $sqlEvidencia);
+                        <div class="col-12 col-md-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">Tabla de Horarios</h4>
+                                </div>
+                                <div class="card-content">
+                                    <div class="card-body">
+                                        <p class="card-text">Por favor, verificar con la tabla superior la atención del
+                                            <code> veterinario</code> una vez que se tenga la información que disponibilidad, cambiar el estado de la hora.
+                                        </p>
+                                    </div>
 
-                                        if($busc -> num_rows >0){
-                                            while($row= mysqli_fetch_array($busc)){                                       
-                                        ?> 
-                                        <tr>
-                                            <td  id="fila-<?php echo $row['idConsultaPer'];?>" ><?php echo $row['idConsultaPer']; ?></td> 
-                                            <td><?php echo $row['cartera']; ?></td>  
-                                            <td><?php echo $row['dni']; ?></td>  
-                                            <td>
-                                                <figure>
-                                                    <img width="250px" height="500px" src="data:image/jpg;base64,<?php echo base64_encode($row['evidenciaPago']); ?>"/>                                      
-                                                </figure> 
-                                                
-                                            </td>
-                                        </tr>
-                                    <?php
-                                        }
-                                    }
+                                    <!-- Table with no outer spacing -->
+                                    <div class="table-responsive">
+                                        <table class="table mb-0 table-lg">
+                                            <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>Hora</th>
+                                                    <th>Disponibilidad</th>
+                                                    <th>Actualizar</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            <?php
+                                            $busc = mysqli_query($con, $sqlhora);
 
-                                    ?>
-                                </tbody>
-                            </table>
+                                            if ($busc->num_rows > 0) {
+                                                while ($row = mysqli_fetch_array($busc)) {
+                                            ?>
+                                                    <tr>
+                                                        <td><?php echo $row['idHora']; ?></td>
+                                                        <td><?php echo $row['hora']; ?></td>
+                                                        <td><?php echo $row['estadoHora']; ?></td>
+                                                        <td>
+                                                            <div class="form-check form-switch">
+                                                                <input class="form-check-input" type="checkbox" id="flexSwitchCheck" checked>
+                                                                <label class="form-check-label" for="flexSwitchCheck">
+                                                                </label>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                            <?php
+                                                }
+                                            }
+                                            ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-                </section>                        
+                </section>
 
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
@@ -196,9 +308,10 @@ include ('barra-lateral.php');
                     </div>
                     </div>
                 </div>
-            </footer>
-        </div>    
-    </div>
+            </footer>  
+        </div>       
+ </div>      
+            
 
 <script>
     function actualizarEstado(idConsultaPer, estadoSeleccionado) {
@@ -247,6 +360,8 @@ include ('barra-lateral.php');
 <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 <script src="assets/vendors/simple-datatables/simple-datatables.js"></script>
+
+
 <script>
     // Simple Datatable
     let table1 = document.querySelector('#table1');
